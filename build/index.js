345f25c5139fb4461bec87130148f1edf3350ca4
Maximilian Sauermann <maximilian.sauermann@gmail.com>
2016-07-10 10:55:29 +0200

    Initial Commit

---
+++ build/index.js
@@ -0,0 +1,54 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _toCamelCase = require('to-camel-case');
+
+var _toCamelCase2 = _interopRequireDefault(_toCamelCase);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var transforms = {
+  'float': 'cssFloat'
+};
+
+function transformProperty(property) {
+  var result = (0, _toCamelCase2.default)(property);
+  result = transforms[result] || result;
+  return result;
+}
+
+function one(element, name, value) {
+  var property = transformProperty(name);
+  var rollback = element.style[property];
+
+  element.style[property] = value;
+  return rollback;
+}
+
+function all(element, css) {
+  var rollback = {};
+
+  Object.keys(css).forEach(function (key) {
+    rollback[key] = one(element, key, css[key]);
+  });
+
+  return rollback;
+}
+
+/**
+ * examples:
+ * style(body, 'background', 'maroon')
+ * style(body, {background: 'maroon', color: 'white'})
+ */
+function style(element) {
+  if (arguments.length === 3) {
+    return one(element, arguments[1], arguments[2]);
+  }
+
+  return all(element, arguments[1]);
+}
+
+exports.default = style;
---
+++ src/index.js
@@ -0,0 +1,46 @@
+import toCamelCase from 'to-camel-case';
+
+const transforms = {
+  'float': 'cssFloat',
+};
+
+function transformProperty(property) {
+  let result = toCamelCase(property);
+  result = transforms[result] || result;
+  return result;
+}
+
+function one(element, name, value) {
+  let property = transformProperty(name);
+  let rollback = element.style[property];
+
+  element.style[property] = value;
+  return rollback;
+}
+
+function all(element, css) {
+  let rollback = {};
+
+  Object
+  .keys(css)
+  .forEach((key) => {
+    rollback[key] = one(element, key, css[key]);
+  });
+
+  return rollback;
+}
+
+/**
+ * examples:
+ * style(body, 'background', 'maroon')
+ * style(body, {background: 'maroon', color: 'white'})
+ */
+function style(element) {
+  if (arguments.length === 3) {
+    return one(element, arguments[1], arguments[2]);
+  }
+
+  return all(element, arguments[1]);
+}
+
+export default style;
